83a85a8932dac96b2ec2eaec6f1340cae8a4f7e4
joe888777 <joe888777@gmail.com>
2021-08-18 01:10:16 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const nextTranslate = require('next-translate');
 module.exports = {
-  reactStrictMode: true,
+  ...nextTranslate()
+  // locales: ['en', 'zh-TW'],
+  // defaultLocale: 'en',
+  // loadLocaleFrom: (lang, ns) =>
+  //   import(`./locales/${lang}/${ns}.json`).then(m => m.default),
+  // "pages": {
+  //   "*": ["common"],
+  //   "/": ["common"]
+  // }
 }
